96aff29e2693216cbb355a2f5045d2f8fa52bba5
juan camilo martinez <juacmartinezl@correo.udistrital.edu.co>
2021-01-27 11:12:07 -0500

    se agrega decodificacion de prueba de png a binario

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,29 @@ function tomarFoto() {
     ).catch((error)=>{
         console.log(error);
     })*/
-    console.log(typeof(picture)); 
+    let data=picture.split(',')[1];
+    console.log(data);
+    this.toBin(data); 
     //document.querySelector('#foto_tomada').src=picture;
+}
+
+function toBin(data){
+    var binArray = []
+    var datEncode = "";
+
+    for (i=0; i < data.length; i++) {
+        binArray.push(data[i].charCodeAt(0).toString(2)); 
+    } 
+    for (j=0; j < binArray.length; j++) {
+        var pad = padding_left(binArray[j], '0', 8);
+        datEncode += pad + ' '; 
+    }
+    function padding_left(s, c, n) { if (! s || ! c || s.length >= n) {
+        return s;
+    }
+    var max = (n - s.length)/c.length;
+    for (var i = 0; i < max; i++) {
+        s = c + s; } return s;
+    }
+    console.log(binArray);
 }
