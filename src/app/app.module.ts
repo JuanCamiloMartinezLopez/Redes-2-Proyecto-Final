f7d9334adcf4a0c328a49313d9810384912f380d
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2021-02-15 19:34:52 -0500

    se agregan componentes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { PaperComponent } from './components/paper/paper.component';
+import { CompressorComponent } from './components/compressor/compressor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    PaperComponent,
+    CompressorComponent
   ],
   imports: [
     BrowserModule,
